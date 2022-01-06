3355e3e077dc399503e4134fbfe85169340e3840
vankenobi <musakucuk99@gmail.com>
2022-01-07 01:51:39 +0300

    Car Pooling algorithm is solved

---
+++ Car Pooling.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def carPooling(self, trips, capacity):
+        _to = []
+        _from = []
+        for i in range(len(trips)):
+            _to.append(trips[i][2])
+            _from.append(trips[i][1])
+        _to = list(set(_to))
+        _from = list(set(_from))
+
+        max_to = max(_to)
+        max_from = max(_from)
+
+        min_to = min(_to)
+        min_from = min(_from)
+
+        live_capacity = 0
+
+        for i in range(min_from,max_to):
+            for j in range(len(trips)):
+                if (trips[j][2] == i):
+                    live_capacity -= trips[j][0]
+            for k in range(len(trips)):
+                if (trips[k][1] == i) :
+                    live_capacity += trips[k][0]
+                if (capacity < live_capacity):
+                    return False
+                
+                
+        return True
+
+        
+        
+        
+                
+
+s1 =  Solution()
+result = s1.carPooling([[4,5,6],[6,4,7],[4,3,5],[2,3,5]],13)
+print(result)
