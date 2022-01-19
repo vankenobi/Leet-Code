fe0fd7e913c1bfc44235730f28aefbba82455b94
vankenobi <musakucuk99@gmail.com>
2022-01-19 20:51:28 +0300

    Longest Common Prefix is solved

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -11,17 +11,20 @@ class Solution(object):
         :rtype: str
         """
         short_vocabulary = min(strs,key=len)
+        strs.remove(short_vocabulary)
         count = 0
-        if(len(short_vocabulary) == 0 ):
-            return strs[0]
+        if(len(short_vocabulary) == 0 ): 
+            return short_vocabulary
         while(count < len(short_vocabulary)):
             char = short_vocabulary[count] 
             for i in strs:
                 if(i[count] != char):
                     return short_vocabulary[:count]
             count +=1
+
+        return short_vocabulary[:count]
         
 
 s1 = Solution()
-result = s1.longestCommonPrefix(["a"])
+result = s1.longestCommonPrefix(["abab","aba",""])
 print(result)
